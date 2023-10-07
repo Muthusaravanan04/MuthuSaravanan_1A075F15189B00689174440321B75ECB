6164d575b791aafb4d00af340ca57266440394bc
Muthusaravanan04 <146065506+Muthusaravanan04@users.noreply.github.com>
2023-10-07 18:38:50 +0530

    Add files via upload

---
+++ challenge for unit 2/bat or bow.py
@@ -0,0 +1,22 @@
+class Player:
+
+  def play(self):
+    print("The player is playing cricket.")
+
+
+class Batsman(Player):
+
+  def play(self):
+    print("The batsman is batting.")
+
+
+class Bowler(Player):
+
+  def play(self):
+    print("The bowler is bowling.")
+
+
+bat = Batsman()
+bow = Bowler()
+bat.play()
+bow.play()
